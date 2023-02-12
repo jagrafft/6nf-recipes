5bf9807db396d8ed8ea32d2d2c2c7fbc335829aa
jagrafft <jgrafft@gmail.com>
2023-02-12 09:51:55 -0600

    Python script writes data tables, created mapping for writing relational tables

--- python/csv-to-sqlite.py
+++ python/csv-to-sqlite.py
@@ -1,4 +1,4 @@
-# import pandas
+import pandas
 import sqlite3
 
 from datetime import datetime
@@ -8,7 +8,21 @@ from subprocess import call
 project_root = Path(__file__).parent.parent
 
 ### Schema for Populating Database ###
-# data
+# Data <- (CSV Column, SQLite Column)
+# Script will generate keys
+# NB. Since all tables use `url_id` as a primary key it is not listed
+data_column_map = {
+    "title": "data_titles",
+    "citation_title": "data_titles",
+    "author": "data_authors",
+    "date": "data_dates",
+    "access_date": "data_dates",
+    "default_mass": "data_default_masses",
+    "url": "data_urls",
+    "ingredient": "data_ingredients",
+    "bakers_percentage": "data_ratios",
+}
+
 # "link"
 ###
 
@@ -21,19 +35,50 @@ if Path.exists(db_path):
 
 # Connect to database, which implicitly creates a new database
 # due to the renaming instructions above
-db = sqlite3.connect(db_path)
+connection = sqlite3.connect(db_path)
 
 # Bootstrap database schema
 call(["sh", "bash/bootstrap_db.sh", db_name])
 ###
 
 ### Load Data from CSVs ###
-# ingredients = pandas.read_csv(project_root / "data" / "ingredient_lists.csv")
-# recipes = pandas.read_csv(project_root / "data" / "recipes.csv")
+ingredients = pandas.read_csv(project_root / "data" / "ingredient_lists.csv")
+recipes = pandas.read_csv(project_root / "data" / "recipes.csv")
 ###
 
-# iterate dataframes...
-#    write to sqlite3 db...
+### Transform data columns ###
+indexed_tuples = {}
+# Push values from CSV columns into an array
+for df in [ingredients, recipes]:
+    for col in df.drop("recipe_id", axis=1):
+        if data_column_map[col] in indexed_tuples:
+            indexed_tuples[data_column_map[col]].extend(df[col].unique())
+        else:
+            indexed_tuples[data_column_map[col]] = [*df[col].unique()]
+
+# Transform arrays into enumerated sets of unique values
+for (k, v) in indexed_tuples.items():
+    indexed_tuples[k] = [(i + 1, v) for (i, v) in enumerate(set(v))]
+
+# Map data values to keys to use for creating relation tables
+data_value_map = {}
+
+for (tbl, vals) in indexed_tuples.items():
+    data_value_map[tbl] = {k: v for (v, k) in vals}
+###
+
+### Populate SQLite database ###
+cursor = connection.cursor()
+
+# Data Tables
+for (tbl, vals) in indexed_tuples.items():
+    cursor.executemany(f"INSERT INTO {tbl} VALUES (?,?)", vals)
+    connection.commit()
+
+# Relation Tables
+
+print(data_value_map)
+###
 
 # install views (and such)...
 
@@ -41,4 +86,7 @@ call(["sh", "bash/bootstrap_db.sh", db_name])
 ### Validate Database Tables ###
 ###
 
+# Close database connection
+cursor.close()
+connection.close()
 # DONE
