f65d65360a3a84d312ca37bba695c50ffcaba54d
jagrafft <jgrafft@gmail.com>
2023-02-12 07:47:08 -0600

    Updated DBML, SQL, code cleanup

--- bash/bootstrap_db.sh
+++ bash/bootstrap_db.sh
@@ -10,23 +10,5 @@ DATABASE="${1}"
 # Create Tables #
 sqlite3 ${DATABASE} < "sql/schema.sql"
 
-# Populate Tables #
-# Tables with no dependencies
-# echo "authors" | xargs -I% sqlite3 ${DATABASE} ".mode csv" ".import --skip 1 data/%.csv %" ".exit"
-# echo "citation_access_dates" | xargs -I% sqlite3 ${DATABASE} ".mode csv" ".import --skip 1 data/%.csv %" ".exit"
-# echo "citation_dates" | xargs -I% sqlite3 ${DATABASE} ".mode csv" ".import --skip 1 data/%.csv %" ".exit"
-# echo "ingredients" | xargs -I% sqlite3 ${DATABASE} ".mode csv" ".import --skip 1 data/%.csv %" ".exit"
-# echo "masses" | xargs -I% sqlite3 ${DATABASE} ".mode csv" ".import --skip 1 data/%.csv %" ".exit"
-# echo "recipes" | xargs -I% sqlite3 ${DATABASE} ".mode csv" ".import --skip 1 data/%.csv %" ".exit"
-
-# Tables with dependencies
-# echo "bakers_percentages" | xargs -I% sqlite3 ${DATABASE} ".mode csv" ".import --skip 1 data/%.csv %" ".exit"
-# echo "citation_titles" | xargs -I% sqlite3 ${DATABASE} ".mode csv" ".import --skip 1 data/%.csv %" ".exit"
-# echo "citation_urls" | xargs -I% sqlite3 ${DATABASE} ".mode csv" ".import --skip 1 data/%.csv %" ".exit"
-# echo "recipe_access_date_ids" | xargs -I% sqlite3 ${DATABASE} ".mode csv" ".import --skip 1 data/%.csv %" ".exit"
-# echo "recipe_author_ids" | xargs -I% sqlite3 ${DATABASE} ".mode csv" ".import --skip 1 data/%.csv %" ".exit"
-# echo "recipe_citation_date_ids" | xargs -I% sqlite3 ${DATABASE} ".mode csv" ".import --skip 1 data/%.csv %" ".exit"
-# echo "recipe_mass_ids" | xargs -I% sqlite3 ${DATABASE} ".mode csv" ".import --skip 1 data/%.csv %" ".exit"
-
 # Create Views
-# sqlite3 ${DATABASE} < views/citations.sql
+# sqlite3 ${DATABASE} < sql/views/...
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,96 +1,97 @@
 -- SQL dump generated using DBML (dbml-lang.org)
 -- Database: MySQL
--- Generated at: 2023-02-05T19:41:10.755Z
--- NB. DBML does not support SQLite, so foreign key references are modified by hand
-
-CREATE TABLE `recipe_urls` (
-  `id` INT PRIMARY KEY,
-  `url` TEXT UNIQUE NOT NULL
-);
+-- Generated at: 2023-02-12T07:17:07.017Z
+-- NB. - DBML does not support SQLite, so foreign key references and other details are modified by hand
+--     - Best to avoid `AUTOINCREMENT` when necessary (https://www.sqlite.org/autoinc.html), so keys will be assigned by Python script
 
 CREATE TABLE `data_authors` (
-  `id` INT PRIMARY KEY,
+  `id` INTEGER PRIMARY KEY,
   `author` TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE `data_dates` (
-  `id` INT PRIMARY KEY,
-  `dte` DATE UNIQUE NOT NULL
+  `id` INTEGER PRIMARY KEY,
+  `dte` TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE `data_ingredients` (
-  `id` INT PRIMARY KEY,
+  `id` INTEGER PRIMARY KEY,
   `ingredient` TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE `data_default_masses` (
-  `id` INT PRIMARY KEY,
+  `id` INTEGER PRIMARY KEY,
   `grams` REAL UNIQUE NOT NULL
 );
 
 CREATE TABLE `data_ratios` (
-  `id` INT PRIMARY KEY,
+  `id` INTEGER PRIMARY KEY,
   `ratio` REAL UNIQUE NOT NULL
 );
 
 CREATE TABLE `data_titles` (
-  `id` INT PRIMARY KEY,
+  `id` INTEGER PRIMARY KEY,
   `title` TEXT UNIQUE NOT NULL
 );
 
+CREATE TABLE `data_urls` (
+  `id` INTEGER PRIMARY KEY,
+  `url` TEXT UNIQUE NOT NULL
+);
+
 CREATE TABLE `recipe_access_dates` (
-  `url_id` INT PRIMARY KEY REFERENCES `recipe_urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  `date_id` INT NOT NULL REFERENCES `data_dates` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `url_id` INTEGER PRIMARY KEY REFERENCES `data_urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  `date_id` INTEGER NOT NULL REFERENCES `data_dates` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `recipe_authors` (
-  `url_id` INT PRIMARY KEY REFERENCES `recipe_urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  `author_id` INT NOT NULL REFERENCES `data_authors` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `url_id` INTEGER PRIMARY KEY REFERENCES `data_urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  `author_id` INTEGER NOT NULL REFERENCES `data_authors` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE `recipe_citation_title` (
-  `url_id` INT PRIMARY KEY REFERENCES `recipe_urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  `title_id` INT NOT NULL REFERENCES `data_titles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE `recipe_citation_titles` (
+  `url_id` INTEGER PRIMARY KEY REFERENCES `data_urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  `title_id` INTEGER NOT NULL REFERENCES `data_titles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `recipe_dates` (
-  `url_id` INT PRIMARY KEY REFERENCES `recipe_urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  `date_id` INT NOT NULL REFERENCES `data_dates` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `url_id` INTEGER PRIMARY KEY REFERENCES `data_urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  `date_id` INTEGER NOT NULL REFERENCES `data_dates` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `recipe_default_masses` (
-  `url_id` INT PRIMARY KEY REFERENCES `recipe_urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  `grams_id` INT NOT NULL REFERENCES `data_default_masses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `url_id` INTEGER PRIMARY KEY REFERENCES `data_urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  `grams_id` INTEGER NOT NULL REFERENCES `data_default_masses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `recipe_ingredients` (
-  `url_id` INT PRIMARY KEY REFERENCES `recipe_urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  `ingredients_id` INT NOT NULL REFERENCES `data_ingredients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `url_id` INTEGER PRIMARY KEY REFERENCES `data_urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  `ingredients_id` INTEGER NOT NULL REFERENCES `data_ingredients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `recipe_ratios` (
-  `url_id` INT PRIMARY KEY REFERENCES `recipe_urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  `ingredients_id` INT NOT NULL REFERENCES `data_ingredients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  `ratio_id` INT NOT NULL REFERENCES `data_ratios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `url_id` INTEGER PRIMARY KEY REFERENCES `data_urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  `ingredients_id` INTEGER NOT NULL REFERENCES `data_ingredients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  `ratio_id` INTEGER NOT NULL REFERENCES `data_ratios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `recipe_titles` (
-  `url_id` INT PRIMARY KEY REFERENCES `recipe_urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  `title_id` INT NOT NULL REFERENCES `data_titles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `url_id` INTEGER PRIMARY KEY REFERENCES `data_urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  `title_id` INTEGER NOT NULL REFERENCES `data_titles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE UNIQUE INDEX `recipe_access_dates_index_0` ON `recipe_access_dates` (`url_id`, `date_id`);
+CREATE UNIQUE INDEX "recipe_access_dates_0" ON "recipe_access_dates" ("url_id", "date_id");
 
-CREATE UNIQUE INDEX `recipe_authors_index_1` ON `recipe_authors` (`url_id`, `author_id`);
+CREATE UNIQUE INDEX "recipe_authors_0" ON "recipe_authors" ("url_id", "author_id");
 
-CREATE UNIQUE INDEX `recipe_citation_title_index_2` ON `recipe_citation_title` (`url_id`, `title_id`);
+CREATE UNIQUE INDEX "recipe_citation_titles_0" ON "recipe_citation_titles" ("url_id", "title_id");
 
-CREATE UNIQUE INDEX `recipe_dates_index_3` ON `recipe_dates` (`url_id`, `date_id`);
+CREATE UNIQUE INDEX "recipe_dates_0" ON "recipe_dates" ("url_id", "date_id");
 
-CREATE UNIQUE INDEX `recipe_default_masses_index_4` ON `recipe_default_masses` (`url_id`, `grams_id`);
+CREATE UNIQUE INDEX "recipe_default_masses_0" ON "recipe_default_masses" ("url_id", "grams_id");
 
-CREATE UNIQUE INDEX `recipe_ingredients_index_5` ON `recipe_ingredients` (`url_id`, `ingredients_id`);
+CREATE UNIQUE INDEX "recipe_ingredients_0" ON "recipe_ingredients" ("url_id", "ingredients_id");
 
-CREATE UNIQUE INDEX `recipe_ratios_index_6` ON `recipe_ratios` (`url_id`, `ingredients_id`, `ratio_id`);
+CREATE UNIQUE INDEX "recipe_ratios_0" ON "recipe_ratios" ("url_id", "ingredients_id", "ratio_id");
 
-CREATE UNIQUE INDEX `recipe_titles_index_7` ON `recipe_titles` (`url_id`, `title_id`);
+CREATE UNIQUE INDEX "recipe_titles_0" ON "recipe_titles" ("url_id", "title_id");
